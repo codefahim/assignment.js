182f2492746582337710955caed8b9e31c36c6eb
Abdullah Al Fahim <50063518+fahimw@users.noreply.github.com>
2021-01-22 00:33:14 +0600

    Problem One Solved

--- assignment.js
+++ assignment.js
@@ -1,1 +1,23 @@
 // https://github.com/codefahim/assignment.js
+
+
+// Problem No One:
+// convert kiloMeter to Meter
+
+// we know, 1 kiloMeter = 1000 meter
+
+
+function kiloMeterToMeter(kiloMeter) {
+    var meter = 1000;
+    var totalMeter = 0;
+    if (kiloMeter < 0) {
+        return "Please Check your input again";
+    } else {
+        totalMeter = meter * kiloMeter;
+    }
+    return totalMeter;
+}
+var result = kiloMeterToMeter(12);
+console.log(result);
+
+// problem one closed
